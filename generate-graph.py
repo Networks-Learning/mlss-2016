42cd0721dbd2572d52473940ef44943a38120728
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-04-23 21:42:05 +0200

    Generate the solution with the graph.

--- generate-graph.py
+++ generate-graph.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import click
-import os
+import os.path as P
 import sys
+import numpy as np
 import networkx as nx
 import random as R
 
 N = 50
 SEED = 42
+
 
 @click.command()
 @click.option('-o', '--output', 'output_path',
@@ -21,7 +23,7 @@ SEED = 42
               default=N,
               help='Number of nodes.')
 @click.option('-p', '--prob', 'edge_prob',
-              default=4.0 / N, # Total number of edges = N², so edges = 4 * N
+              default=4.0 / N, # Total number of edges = N^2, so edges = 4 * N
               help='Fraction of edges which should exist')
 @click.option('-f', '--force',
               is_flag=True,
@@ -29,8 +31,17 @@ SEED = 42
 @click.option('--seed',
               default=SEED,
               help='Seed for graph generation.')
-def run(output_path, graph_type, force, seed, num_nodes, edge_prob):
-    if os.path.exists(output_path) and not force:
+@click.option('-S', '--solution', 'solution_path',
+              prompt='Which file to save solution to',
+              default='solution.csv',
+              help='The path where to save the solution (as a CSV file).'
+                   'If the filename ends in .gz, the file will be GZipped.')
+def run(output_path, graph_type, force,
+        seed, num_nodes, edge_prob, solution_path):
+
+    any_op_file_exists = (P.exists(output_path) or P.exists(solution_path))
+
+    if any_op_file_exists and not force:
         print('Cannot overwrite without --force', file=sys.stderr)
         sys.exit(-1)
 
@@ -42,11 +53,14 @@ def run(output_path, graph_type, force, seed, num_nodes, edge_prob):
         print('Unknown graph type: ', graph_type, file=sys.stderr)
         sys.exit(-1)
 
+    A = np.zeros((num_nodes, num_nodes), dtype='float')
     # All edges are given uniformly random weights.
     for u, v, d in g.edges(data=True):
         d['act_prob'] = R.random()
+        A[u, v] = d['act_prob']
 
     nx.write_edgelist(g, output_path)
+    np.savetxt(solution_path, A, delimiter=',')
 
 
 if __name__ == '__main__':
