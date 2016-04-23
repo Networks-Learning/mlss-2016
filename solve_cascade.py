9c2a8ac88be5101281067598c0a0de865dcd1d7a
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-04-23 22:24:11 +0200

    Change the solution methods to run interactively.
    Also, add more comments and verify solution quality.

--- solve_cascade.py
+++ solve_cascade.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import click
-import os
-import sys
 import csv
 import numpy as np
 import cvxpy as CVX
-import networkx as nx
 from collections import defaultdict
+
+# This file has been changed from a script friendly format to an interactive
+# session friendly version.
 
 
 # @click.command()
@@ -20,86 +19,89 @@ from collections import defaultdict
 #               prompt='Time horizon.',
 #               default=1.0,
 #               help='Time horizon (after which, no data was collected).')
-def run_with_cvx(input_file, time_period, num_nodes):
-    cascades = defaultdict(lambda : [])
+# def run_with_cvx(input_file, time_period, num_nodes):
 
-    # Reading data
-    for row in csv.DictReader(input_file):
-        # keys = 'cascade_id', 'dst', 'at'
-        cascade_id = row['cascade_id']
-        dst        = int(row['dst'])
-        at         = float(row['at'])
-        assert at < time_period, "Infection after observation period."
-        cascades[cascade_id].append((at, dst))
+input_file = open('cascades.csv', 'r')
+time_period = 1.0
+num_nodes = 50
 
-    # Start definition of the problem
+cascades = defaultdict(lambda : [])
 
-    # Sort according to time
-    for cascade_id in cascades.keys():
-        cascades[cascade_id] = sorted(cascades[cascade_id])
+# Reading data
+for row in csv.DictReader(input_file):
+    # keys = 'cascade_id', 'dst', 'at'
+    cascade_id = row['cascade_id']
+    dst        = int(row['dst'])
+    at         = float(row['at'])
+    assert at < time_period, "Infection after observation period."
+    cascades[cascade_id].append((at, dst))
 
-    # Possible edges
-    possible_edges = set()
-    for c in cascades.values():
-        for i in range(len(c)):
-           for j in range(i):
-               possible_edges.add((c[j][1], c[i][1]))
+# Start definition of the problem
 
-    # Formulating the problem for each row of influence matrix A
-    A = np.zeros((num_nodes, num_nodes), dtype=float)
-    probs = []
-    results = []
+# Sort according to time
+for cascade_id in cascades.keys():
+    cascades[cascade_id] = sorted(cascades[cascade_id])
 
-    # These problems can be solved in parallel
-    for target_node in range(num_nodes):
-        Ai = CVX.Variable(num_nodes, name='A[:, {}]'.format(target_node))
+# Possible edges
+possible_edges = set()
+for c in cascades.values():
+    for i in range(len(c)):
+       for j in range(i):
+           possible_edges.add((c[j][1], c[i][1]))
 
-        # Which edges are constrained to be zero
-        constraints = []
-        for j in range(num_nodes):
-            if (j, target_node) not in possible_edges:
-                constraints.append(Ai[j] == 0)
-            else:
-                constraints.append(Ai[j] >= 0)
+# Formulating the problem for each row of influence matrix A
+A = np.zeros((num_nodes, num_nodes), dtype=float)
+probs = []
+results = []
 
-        expr = 0
-        for c_idx, c in cascades.items():
-            infection_time_arr = [x[0] for x in c if x[1] == target_node]
+# These problems can be solved in parallel
+for target_node in range(num_nodes):
+    Ai = CVX.Variable(num_nodes, name='A[:, {}]'.format(target_node))
 
-            assert len(infection_time_arr) <= 1
-
-            if len(infection_time_arr) == 0:
-                # Node 'i' was not infected in this cascade
-                # NICE-TO-HAVE: Can be precomputed and vectorized
-                for j in range(len(c)):
-                    expr += Ai[c[j][1]] * (c[j][0] - time_period)
-            else:
-                # Node 'i' was infected in this cascade
-                # NICE-TO-HAVE: Vectorize
-                infection_time = infection_time_arr[0]
-                num_infected_before = 0
-                log_sum = 0
-                for j in range(len(c)):
-                    if c[j][0] < infection_time:
-                        num_infected_before += 1
-                        expr += Ai[c[j][1]] * (c[j][0] - infection_time)
-                        log_sum += Ai[c[j][1]]
-                    else:
-                        break
-
-                if num_infected_before > 0:
-                    # This check avoids log(0) terms
-                    expr += CVX.log(log_sum)
-
-        prob = CVX.Problem(CVX.Maximize(expr), constraints)
-        res = prob.solve(verbose=True)
-        probs.append(prob)
-        results.append(res)
-        if prob.status in [CVX.OPTIMAL, CVX.OPTIMAL_INACCURATE]:
-            A[:, target_node] = np.asarray(Ai.value).squeeze()
+    # Which edges are constrained to be zero
+    constraints = []
+    for j in range(num_nodes):
+        if (j, target_node) not in possible_edges:
+            constraints.append(Ai[j] == 0)
         else:
-            A[:, target_node] = -1
+            constraints.append(Ai[j] >= 0)
 
-    return A, probs, results
+    expr = 0
+    for c_idx, c in cascades.items():
+        infection_time_arr = [x[0] for x in c if x[1] == target_node]
 
-# TODO: Add code for comparing the performance.
+        assert len(infection_time_arr) <= 1
+
+        if len(infection_time_arr) == 0:
+            # Node 'i' was not infected in this cascade
+            # NICE-TO-HAVE: Can be precomputed and vectorized
+            for j in range(len(c)):
+                expr += Ai[c[j][1]] * (c[j][0] - time_period)
+        else:
+            # Node 'i' was infected in this cascade
+            # NICE-TO-HAVE: Vectorize
+            infection_time = infection_time_arr[0]
+            num_infected_before = 0
+            log_sum = 0
+            for j in range(len(c)):
+                if c[j][0] < infection_time:
+                    num_infected_before += 1
+                    expr += Ai[c[j][1]] * (c[j][0] - infection_time)
+                    log_sum += Ai[c[j][1]]
+                else:
+                    break
+
+            if num_infected_before > 0:
+                # This check avoids log(0) terms
+                expr += CVX.log(log_sum)
+
+    prob = CVX.Problem(CVX.Maximize(expr), constraints)
+    res = prob.solve(verbose=True)
+    probs.append(prob)
+    results.append(res)
+    if prob.status in [CVX.OPTIMAL, CVX.OPTIMAL_INACCURATE]:
+        A[:, target_node] = np.asarray(Ai.value).squeeze()
+    else:
+        A[:, target_node] = -1
+
+# return A, probs, results
--- solve_cascade_no_cvx.py
+++ solve_cascade_no_cvx.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 from __future__ import print_function
-import click
-import os
-import sys
 import csv
 import numpy as np
-import networkx as nx
 from scipy import optimize as O
 from collections import defaultdict
 
+
+# This file has been changed from a script friendly format to an interactive
+# session friendly version.
+
+
 def f_factory(Ai_coef, Ai_log_coefs, Ai_mask):
+    """
+    Returns a pair of functions which calculate the negative log-likelihood
+    and the gradient respectively.
+
+    Ai_mask is used to work only on the variables which are not constrained to
+    be zero.
+
+    L-BFGS-B does not handle variables which are bounded to be a constant well.
+    """
+
     def f(x):
         v = Ai_coef[Ai_mask].dot(x)
         # TODO: Can be vectorized better
@@ -28,90 +39,97 @@ def f_factory(Ai_coef, Ai_log_coefs, Ai_mask):
     return f, f_prime
 
 
-def run_no_cvx(input_file, time_period, num_nodes):
-    cascades = defaultdict(lambda : [])
+# def run_no_cvx(input_file, time_period, num_nodes):
 
-    # Reading data
-    for row in csv.DictReader(input_file):
-        # keys = 'cascade_id', 'dst', 'at'
-        cascade_id = int(row['cascade_id'])
-        dst        = int(row['dst'])
-        at         = float(row['at'])
-        assert at < time_period, "Infection after observation period."
-        cascades[cascade_id].append((at, dst))
+input_file = open('cascades.csv', 'r')
+time_period = 1.0
+num_nodes = 50
 
-    # Start definition of the problem
+cascades = defaultdict(lambda : [])
 
-    # Sort according to time
-    cascades_keys = sorted(cascades.keys())
-    for cascade_id in cascades_keys:
-        cascades[cascade_id] = sorted(cascades[cascade_id])
+# Reading data
+for row in csv.DictReader(input_file):
+    # keys = 'cascade_id', 'dst', 'at'
+    cascade_id = int(row['cascade_id'])
+    dst        = int(row['dst'])
+    at         = float(row['at'])
+    assert at < time_period, "Infection after observation period."
+    cascades[cascade_id].append((at, dst))
 
-    # Possible edges
-    possible_edges = set()
-    for c in cascades.values():
-        for i in range(len(c)):
-           for j in range(i):
-               possible_edges.add((c[j][1], c[i][1]))
+# Start definition of the problem
 
-    A = np.zeros((num_nodes, num_nodes), dtype=float)
-    statuses = []
-    results = []
+# Sort according to time
+cascades_keys = sorted(cascades.keys())
+for cascade_id in cascades_keys:
+    cascades[cascade_id] = sorted(cascades[cascade_id])
 
-    eps = 1e-14
+# Possible edges
+possible_edges = set()
+for c in cascades.values():
+    for i in range(len(c)):
+       for j in range(i):
+           possible_edges.add((c[j][1], c[i][1]))
 
-    for target_node in range(num_nodes):
-        Ai_init = np.zeros(num_nodes, dtype=float) + 1.0
-        Ai_coef = np.zeros_like(Ai_init)
+A = np.zeros((num_nodes, num_nodes), dtype=float)
+statuses = []
+results = []
 
-        Ai_log_coefs = [np.zeros((num_nodes,), dtype=float)
-                        for _ in cascades_keys]
+# Placing a bound of 0 causes problems if the function evaluation results
+# in calculating the logarithm of a 0 term.
+eps = 1e-14
 
-        Ai_mask = np.array([ (j, target_node) in possible_edges
-                              for j in range(num_nodes) ], dtype=bool)
-        Ai_constraints = [(eps, None)] * Ai_mask.sum()
+for target_node in range(num_nodes):
+    Ai_init = np.zeros(num_nodes, dtype=float) + 1.0
+    Ai_coef = np.zeros_like(Ai_init)
 
-        for c_idx, key in enumerate(cascades_keys):
-            c = cascades[key]
-            infection_time_arr = [x[0] for x in c if x[1] == target_node]
+    Ai_log_coefs = [np.zeros((num_nodes,), dtype=float)
+                    for _ in cascades_keys]
 
-            assert len(infection_time_arr) <= 1
+    Ai_mask = np.array([ (j, target_node) in possible_edges
+                          for j in range(num_nodes) ], dtype=bool)
+    Ai_constraints = [(eps, None)] * Ai_mask.sum()
 
-            if len(infection_time_arr) == 0:
-                # Node wasn't infected
-                for j in range(len(c)):
-                    Ai_coef[c[j][1]] += (c[j][0] - time_period)
-            else:
-                infection_time = infection_time_arr[0]
+    for c_idx, key in enumerate(cascades_keys):
+        c = cascades[key]
+        infection_time_arr = [x[0] for x in c if x[1] == target_node]
 
-                for j in range(len(c)):
-                    if c[j][0] < infection_time:
-                        Ai_coef[c[j][1]] += (c[j][0] - infection_time)
-                        Ai_log_coefs[c_idx][c[j][1]] += 1.0
-                    else:
-                        break
+        assert len(infection_time_arr) <= 1
 
-        assert np.all(Ai_coef <= 0)
-
-        f, f_prime = f_factory(Ai_coef, Ai_log_coefs, Ai_mask)
-
-        # print('Initial f(x) = {}'.format(f(Ai_init[Ai_mask])))
-        # print('Initial f_prime(x) = {}'.format(f_prime(Ai_init[Ai_mask])))
-        # print('Initial Ai_coef = {}'.format(Ai_coef))
-
-        if Ai_mask.sum() > 0:
-            [Ai, res, status] = O.fmin_l_bfgs_b(f, Ai_init[Ai_mask], fprime=f_prime,
-                                                disp=2, bounds=Ai_constraints, factr=1e07)
-            A[Ai_mask, target_node] = Ai
+        if len(infection_time_arr) == 0:
+            # Node wasn't infected
+            for j in range(len(c)):
+                Ai_coef[c[j][1]] += (c[j][0] - time_period)
         else:
-            Ai = Ai_init
-            res = - np.inf
-            status = { 'error': 'No possible edges to this node.' }
+            infection_time = infection_time_arr[0]
 
-        statuses.append(status)
-        results.append(res)
+            for j in range(len(c)):
+                if c[j][0] < infection_time:
+                    Ai_coef[c[j][1]] += (c[j][0] - infection_time)
+                    Ai_log_coefs[c_idx][c[j][1]] += 1.0
+                else:
+                    break
 
-    return A, statuses, results
+    assert np.all(Ai_coef <= 0)
+
+    f, f_prime = f_factory(Ai_coef, Ai_log_coefs, Ai_mask)
+
+    # print('Initial f(x) = {}'.format(f(Ai_init[Ai_mask])))
+    # print('Initial f_prime(x) = {}'.format(f_prime(Ai_init[Ai_mask])))
+    # print('Initial Ai_coef = {}'.format(Ai_coef))
+
+    if Ai_mask.sum() > 0:
+        [Ai, res, status] = O.fmin_l_bfgs_b(f, Ai_init[Ai_mask], fprime=f_prime,
+                                            disp=2, bounds=Ai_constraints, factr=1e07)
+        A[Ai_mask, target_node] = Ai
+    else:
+        Ai = Ai_init
+        res = - np.inf
+        status = { 'error': 'No possible edges to this node.' }
+
+    statuses.append(status)
+    results.append(res)
+
+# return A, statuses, results
 
 
 
