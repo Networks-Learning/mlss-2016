2527cd50207f532f7d520fb2aa2b29a0657d1c52
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-05-18 17:39:20 +0200

    Handle the case when there are no events for a cascade.

--- recurrent_events/Hawkes_log_lik.py
+++ recurrent_events/Hawkes_log_lik.py
@@ -4,7 +4,7 @@ import cvxpy as CVX
 def Hawkes_log_lik(T, alpha_opt, lambda_opt, lambda_ti, survival, for_cvx=False):
     L = 0
     for i in range(len(lambda_ti)):
-        if for_cvx:
+        if for_cvx and len(lambda_ti) > 0:
             L += CVX.sum_entries(CVX.log(lambda_opt + alpha_opt * lambda_ti[i]))
         else:
             L += np.sum(np.log(lambda_opt + alpha_opt * lambda_ti[i]))
