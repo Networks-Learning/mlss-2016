cf0478c0e3daafdfaa27abfab3f1dce7b184a01e
ivaleraM <ivalera@sslvpn-163-203.mpi-sws.org>
2016-05-19 10:33:17 +0200

    score calculation matlab

--- graph_inference/calc_score.m
+++ graph_inference/calc_score.m
@@ -1,1 +1,13 @@
-# TODO: Write metrics for MATLAB.
+function [F1, precision, recall] = calc_score(A_pred, A_soln, eps)
+
+num_edges = sum(sum(A_soln > eps));
+TP = sum(sum(A_soln(A_pred > eps) >= eps));
+FP = sum(sum(A_soln(A_pred > eps) < eps));
+FN = sum(sum(A_pred(A_soln > eps) < eps));
+
+precision = TP / (TP + FP);
+recall = TP / num_edges;
+
+F1 = 2 * TP / (2 * TP + FN + FP);
+
+end
